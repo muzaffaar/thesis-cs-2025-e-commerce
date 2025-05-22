eea433e8ceb6dd9f36e9f235694c0255e525df9c
Muzaffar Tursunov <muzaffar-shoshiy@mailbox.unideb.hu>
2025-05-22 16:31:20 +0200

    docs(api): Comment added to Loginable interface

--- app/Http/Controllers/Api/Auth/Loginable.php
+++ app/Http/Controllers/Api/Auth/Loginable.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Api\Auth;
 
 use Illuminate\Http\Request;
 
+/**
+ * This class defines how Socialite login controllers should be.
+ * E.g, Google, Facebook, Amazon, Apple, ... login controllers should have those methods.
+ */
 interface Loginable
 {
     public function redirect() : string;
